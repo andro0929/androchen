442132b10ab0bd89a1cc744d6584d733a0f0eca1
andro0929 <andro0929@gmail.com>
2013-04-28 11:03:25 +0800

    v1.0.2 Use CSS Sprite for icons

--- application/views/_sidebar.php
+++ application/views/_sidebar.php
@@ -1,36 +1,42 @@
+<?php
+    $sprite_img_src = "img/icon_sprite.png";
+    $href_gmail = "mailto:andro0929@gmail.com";
+    $href_fb = "http://www.facebook.com/AndroChen0929";
+    $href_gplus = "https://plus.google.com/118026338822127492529/about";
+    $href_linkedin = "http://www.linkedin.com/in/androchen";
+    $href_github = "https://github.com/andro0929";
+    $href_blogger = "http://androchen.blogspot.tw/";
+    $href_wordpress = "http://androchen.qov.tw/wordpress";
+    $href_portfolio = base_url("asset/portfolio_AndroChen.pdf");
+?>
 <!--Sidebar content-->
 <div id="sidebar-andro-img"><img src="img/andro.jpg" alt="Andro Chen"></div>
 <div id="sidebar-andro-name">陳俊安 <span>Chen Chun-An</span></div>
-<div id="sidebar-social-info">
-    <a href='mailto:andro0929@gmail.com' 
-    	title="Send email to Andro">
-    	<img src="img/icon/gmail.png" target="blank" alt="Gmail"></a>
-    <a href='http://www.facebook.com/AndroChen0929' 
+<div class="sidebar-social-info">
+    <a href="<?=$href_gmail?>" id="sidebar-icon-gmail"
+    	title="Send email to Andro" target="blank">
+    	<img src="<?=$sprite_img_src?>" alt="Gmail"></a>
+    <a href="<?=$href_fb?>" id="sidebar-icon-fb"
     	title="Find Andro on Facebook" target="blank">
-    	<img src="img/icon/fb.gif" alt="Facebook"></a>
-    <a href='https://plus.google.com/118026338822127492529/about' 
-    	title="Find Andro on Google+" target="blank">
-    	<img src="img/icon/google+.png" alt="Google+"></a>
-    <a href='http://www.linkedin.com/in/androchen' 
+    	<img src="<?=$sprite_img_src?>" alt="Facebook"></a>
+    <a href="<?=$href_gplus?>" id="sidebar-icon-gplus" 
+        title="Find Andro on Google+" target="blank">
+    	<img src="<?=$sprite_img_src?>" alt="Google+"></a>
+    <a href="<?=$href_linkedin?>" id="sidebar-icon-linkedin"
     	title="Find Andro on LinkedIn" target="blank">
-    	<img src="img/icon/linkedin.png" alt="LinkedIn"></a>
-    <a href='https://github.com/andro0929' 
+    	<img src="<?=$sprite_img_src?>" alt="LinkedIn"></a>
+    <a href="<?=$href_github?>" id="sidebar-icon-github"
     	title="Find Andro on Github" target="blank">
-    	<img src="img/icon/github.png" alt="Github"></a>
+    	<img src="<?=$sprite_img_src?>" alt="Github"></a>
 </div>
-<div class="sidebar-mysite">
-    <a href='http://androchen.blogspot.tw/' 
-    	title="My Blogger - Andro Tips" target="blank">
-    	<img src="img/icon/blogger.png" alt="Andro Tips"> Andro Tips</a>
+<div class="sidebar-social-info">    
+    <a href="<?=$href_blogger?>" id="sidebar-icon-blogger"
+        title="My Blogger - Andro Tips" target="blank">
+        <img src="<?=$sprite_img_src?>" alt="Andro Tips"></a>
+    <a href="<?=$href_wordpress?>" id="sidebar-icon-wordpress"
+        title="My Wordpress - Andro Says" target="blank">
+        <img src="<?=$sprite_img_src?>" alt="Andro Says"></a>
+    <a href="<?=$href_portfolio?>" id="sidebar-icon-portfolio"
+        title="My Portfolio" target="blank">
+        <img src="<?=$sprite_img_src?>" alt="My Portfolio"></a>
 </div>
-<div class="sidebar-mysite">
-    <a href='http://androchen.qov.tw/wordpress' 
-    	title="My Wordpress - Andro Say" target="blank">
-    	<img src="img/icon/wordpress.png" alt="Andro Say"> Andro Say</a>
-</div> 
-<div id="sidebar-portfolio" class="sidebar-mysite">
-    <a href='<?=base_url("asset/portfolio_AndroChen.pdf")?>' 
-        title="My Resume" target="blank">
-        <img src="img/icon/resume.png" alt="Resume"> <a href='<?=base_url("asset/portfolio_AndroChen.pdf")?>' title="My Portfolio" target="blank">
-        My Portfolio</a>
-</div> 
